25dc12cd398ace437e62ef8730c67967e4f1678e
nikitha <nikitaamu@gmail.com>
2018-03-02 00:39:48 +0530

    changed verify token for get request

--- controllers/verification.js
+++ controllers/verification.js
@@ -1,7 +1,7 @@
 module.exports = (req, res) => {
  const hubChallenge = req.query['hub.challenge'];
 const hubMode = req.query['hub.mode'];
-const verifyTokenMatches = (req.query['hub.verify_token'] === HACK);
+const verifyTokenMatches = (req.query['hub.verify_token'] === process.env.VERIFICATION_TOKEN);
 
 if (hubMode && verifyTokenMatches) {
  res.status(200).send(hubChallenge);
