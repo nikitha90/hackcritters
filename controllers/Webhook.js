0f43e3d39db85cf19f247bea359e43f8190faff7
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2020-10-03 00:16:43 +0530

    structured the code and changed few variables for readability

--- controllers/Webhook.js
+++ controllers/Webhook.js
@@ -1,15 +1,16 @@
-const processMessage = require('../services/messengerService');
+const processMessage = require("../services/messengerService");
+const CONSTANTS = require("../CONSTANTS");
 module.exports = (req, res) => {
- if (req.body.object === 'page') {
- 	console.log('received message from facebook');
- req.body.entry.forEach(entry => {
- entry.messaging.forEach(event => {
- if (event.text) {
- console.log('text message received');
- processMessage(event);
- }
- });
- });
-res.status(200).end();
- }
+  if (req.body.object === CONSTANTS.PAGE) {
+    console.log("received message from facebook");
+    req.body.entry.forEach((entry) => {
+      entry.messaging.forEach((event) => {
+        if (event.text) {
+          console.log("text message received");
+          processMessage(event);
+        }
+      });
+    });
+    res.status(200).end();
+  }
 };
--- services/messengerService.js
+++ services/messengerService.js
@@ -1,33 +1,35 @@
-const API_AI_TOKEN = '992326f1c67f4df4b48b1c468b5c85cf';
-const apiAiClient = require('apiai') (API_AI_TOKEN);
-const FACEBOOK_ACCESS_TOKEN = process.env.VERIFICATION_TOKEN;
-const request = require('request');
+const apiAiClient = require("apiai")(API_AI_TOKEN);
+const request = require("request");
+const CONSTANTS = require("../CONSTANTS");
 
+const FACEBOOK_ACCESS_TOKEN = CONSTANTS.VERIFICATION_TOKEN;
+const API_AI_TOKEN = CONSTANTS.API_AI_TOKEN;
 
 const sendTextMessage = (senderId, text) => {
- request({
-		 url: 'https://graph.facebook.com/v2.6/me/messages',
-		 qs: { access_token: FACEBOOK_ACCESS_TOKEN },
-		 method: 'POST',
-		 json: {
-		 recipient: { id: senderId },
-		 message: { text },
-		 }
- 		});
+  request({
+    url: CONSTANTS.FB_URL,
+    qs: { access_token: FACEBOOK_ACCESS_TOKEN },
+    method: CONSTANTS.POST,
+    json: {
+      recipient: { id: senderId },
+      message: { text },
+    },
+  });
 };
 
 module.exports = (event) => {
-		 const senderId = event.sender.id;
-		 const message = event.text;
-		const apiaiSession = apiAiClient.textRequest(message, {sessionId: 'CustomerServiceAgent'});
-		apiaiSession.on('response', (response) => {
-		 const result = response.result.fulfillment.speech;
-
-		 apiaiSession.on('response', (response) => {
-		 const result = response.result.fulfillment.speech;
-		sendTextMessage(senderId, result);
-		 });
-		apiaiSession.on('error', error => console.log(error));
-		 apiaiSession.end();
-});
-	}
+  const senderId = event.sender.id;
+  const message = event.text;
+  const apiaiSession = apiAiClient.textRequest(message, {
+    sessionId: CONSTANTS.CUSTOMER_SERVICE_AGENT,
+  });
+  apiaiSession.on(CONSTANTS.RESPONSE, (response) => {
+    const result = response.result.fulfillment.speech;
+    apiaiSession.on(CONSTANTS.RESPONSE, (response) => {
+      const result = response.result.fulfillment.speech;
+      sendTextMessage(senderId, result);
+    });
+    apiaiSession.on(CONSTANTS.ERROR, (error) => console.log(error));
+    apiaiSession.end();
+  });
+};
